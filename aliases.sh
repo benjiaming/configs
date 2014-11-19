e1bcfb93b94113e1f6ff157e4d31ecade1746aee
Ben Blazke <bblazke@23andme.com>
2014-11-19 11:35:27 -0800

    Support for zsh.

---
+++ aliases.sh
@@ -0,0 +1,40 @@
+#
+# aliases
+#
+alias anscheck="ansible-playbook --syntax-check --list-tasks"
+alias anv="ansible-playbook -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory --private-key=~/.ssh/ansible-insecure"
+alias ea="vi ~/git/config/aliases"
+alias ei="edit_in_idea"
+alias eb="vi ~/.bashrc"
+alias eg="vi ~/.gitconfig"
+alias ep="edit_in_pycharm"
+alias ez="vi ~/.zshrc"
+alias fgr="find .| grep -i --color"
+alias ga="git add"
+alias gh="history | grep --color"
+alias gis="git status"
+alias gp="git pull"
+alias grpo='git remote prune origin'
+alias gs="git status"
+alias ll='ls -lahF'
+alias l='ll'
+alias resource='source ~/.bashrc'
+alias tei="touch_and_edit_in_idea"
+alias vp='vagrant provision'
+alias vr='vagrant reload'
+alias vs='vagrant ssh'
+alias vsc='vagrant ssh-config'
+alias vu='vagrant up'
+alias vup='vagrant up --provision'
+
+#
+# aliases for paths
+#
+alias ans="cd ~/git/ansible"
+alias cur='cd ~/git/current'
+alias ttam='cd ~/git/current'
+alias gc="cd ~/git/config"
+
+
+
+
